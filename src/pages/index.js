716e8f98d8693bbd7d9f08e5cbd165303bad3390
Odion Akhes <akhetuamhenodion@gmail.com>
2023-03-17 08:58:01 +0100

    products

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from './HomePage'
-import Products from './ProductsPage'
-import SingleProduct from './SingleProductPage'
-import About from './AboutPage'
-import Error from './ErrorPage'
-
-
-export { Home, SingleProduct, Error, About, Products };
